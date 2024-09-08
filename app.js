f3f0b368e6f0d4306145b4f770a6012822682cd7
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-09-08 17:28:10 -0300

    090824 aula05

--- app.js
+++ app.js
@@ -2,13 +2,31 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    //
+    if (!campoPesquisa) {
+        section.innerHTML = "Nada foi encontrado!"
+        return;
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
     // Inicializa uma string vazia para armazenar os resultados da pesquisa
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre cada elemento no array de dados
     for (let dado of dados) {
-        // Constrói o HTML para cada item do resultado da pesquisa, formatando o título, descrição e link com base nos dados do objeto
-        resultados += `
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+        //
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // Constrói o HTML para cada item do resultado da pesquisa, formatando o título, descrição e link com base nos dados do objeto
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
@@ -17,6 +35,11 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
         `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado! Digite o nome de um atleta ou esporte.</p>"
     }
 
     // Atribui o HTML gerado para o conteúdo interno da seção
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
         titulo : "Rayssa Leal",
         descricao : "Rayssa Leal, a Fadinha, é uma skatista brasileira que, aos 13 anos, se tornou a mais jovem medalhista olímpica da história do Brasil.",
-        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags : "skate fada sk8"
     },
     {
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, agilidade e determinação, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "judô judo judoka"
     },
     {
         titulo: "Beatriz Souza",
         descricao: "Beatriz Souza é uma judoca brasileira que vem conquistando cada vez mais destaque no cenário internacional. Sua trajetória é marcada por dedicação, talento e uma força de vontade impressionante. E conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024.",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags : "ginastica ouro ginástica"
     }
 ];
