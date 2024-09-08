08b07d2232821db3633bc66aebb66bc1362acbb2
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-09-08 15:11:04 -0300

    090824 aula03

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, a Fadinha, é uma skatista brasileira que, aos 13 anos, se tornou a mais jovem medalhista olímpica da história do Brasil.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, agilidade e determinação, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    }
+];
